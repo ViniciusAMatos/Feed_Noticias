8d5648a2d8d0fa7de0bbef6a7f3040ff1ba24030
Vinicius Matos <34461311+VinciusAndre@users.noreply.github.com>
2020-07-12 11:57:16 -0300

    Revert "Construção e Finalização da tela de login e cadastro"
    This reverts commit 57a9c21389899a368905527001bec487939ea590.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
-/*
- * Author: Ciro Dourado de Oliveira
- */
-
+import 'package:feed_noticias/news_screen.dart';
 import 'package:flutter/material.dart';
-
-import 'myapp.dart';
-
-/*
- * async é usado para configurar o Refresh da página
- */
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home: Newscreen(getCategory: 'Sports',geTitle: 'Esportes',),//Passando o parametro da tela, que sera o search.
+    );
+  }
 }
--- lib/news_screen.dart
+++ lib/news_screen.dart
@@ -4,18 +4,20 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:link/link.dart';
 
+
 class Newscreen extends StatefulWidget {
 //Criando variáveis e construtores para pegarmos o título da tela e também a categoria selecionada.
-  final String geTitle;
-  final String getCategory;
+   final String geTitle;
+   final String getCategory;
 
-  Newscreen({this.geTitle, this.getCategory});
+   Newscreen({this.geTitle, this.getCategory});
 
   @override
   _NewscreenState createState() => _NewscreenState();
 }
 
 class _NewscreenState extends State<Newscreen> {
+
   @override
   void initState() {
     super.initState();
@@ -38,46 +40,46 @@ class _NewscreenState extends State<Newscreen> {
         itemBuilder: (context, index) {
           return Link(
               url: snapshot.data['articles'][index]['url'],
-              onError: _showErrorSnackBar, //Era pra funcionar, mas...
+              onError: _showErrorSnackBar,//Era pra funcionar, mas...
               child: Card(
-                child: Container(
-                  height: 100.0,
-                  width: 400.0,
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        child: Image.network(_verification(snapshot, index),
-                            fit: BoxFit.cover),
-                        width: MediaQuery.of(context).size.width * 0.42,
-                      ), //Pegar imagem da noticia da API.
-                      Container(
-                        margin: EdgeInsets.only(left: 10),
-                        width: 200.0,
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.only(
-                                top: 5,
-                              ),
-                              child: Text(
-                                snapshot.data['articles'][index]['title'],
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 5,
-                                textAlign: TextAlign.justify,
-                                style: TextStyle(
-                                  letterSpacing: 0.5,
-                                  wordSpacing: 0.5,
-                                ),
-                              ), // Pegar o titulo da notica da API.
-                              //Text(snapshot.data['articles'][index]['description'], overflow: TextOverflow.fade)// Pegar a descrição da noticia.
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ));
+            child: Container(
+              height: 100.0,
+              width: 400.0,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Image.network(_verification(snapshot, index),
+                        fit: BoxFit.cover),
+                    width: MediaQuery.of(context).size.width * 0.42,
+                  ), //Pegar imagem da noticia da API.
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    width: 200.0,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 5,
+                          ),
+                          child: Text(
+                            snapshot.data['articles'][index]['title'],
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 5,
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              letterSpacing: 0.5,
+                              wordSpacing: 0.5,
+                            ),
+                          ), // Pegar o titulo da notica da API.
+                          //Text(snapshot.data['articles'][index]['description'], overflow: TextOverflow.fade)// Pegar a descrição da noticia.
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ));
         });
   }
 
@@ -88,8 +90,7 @@ class _NewscreenState extends State<Newscreen> {
       return 'https://www.lumitecfoto.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/f/u/fundo-fotogr_fico-em-tecido-muslin-20x30m-preto-3.jpg';
   }
 
-  void _showErrorSnackBar() {
-    //Funcao erro modelo da doc do Flutter, caso nao consiga carregar a URL.
+void _showErrorSnackBar() {//Funcao erro modelo da doc do Flutter, caso nao consiga carregar a URL.
     Scaffold.of(context).showSnackBar(
       SnackBar(
         backgroundColor: Colors.red,
@@ -97,6 +98,9 @@ class _NewscreenState extends State<Newscreen> {
       ),
     );
   }
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -114,7 +118,10 @@ class _NewscreenState extends State<Newscreen> {
                 switch (snapshot.connectionState) {
                   case ConnectionState.waiting:
                   case ConnectionState.none:
-                    return Center(
+                    return Container(
+                      width: 200.0,
+                      height: 200.0,
+                      alignment: Alignment.center,
                       child: CircularProgressIndicator(
                         valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
                         strokeWidth: 5.0,
